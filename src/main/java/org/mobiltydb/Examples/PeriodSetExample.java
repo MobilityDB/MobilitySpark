f5de6a62ad2e9774494239af6d7c6f64bf3aec48
satriabw <satria_bagus@hotmail.com>
2023-08-08 13:45:30 +0200

    Add period set UDT

---
+++ src/main/java/org/mobiltydb/Examples/PeriodSetExample.java
@@ -0,0 +1,64 @@
+package org.mobiltydb.Examples;
+
+import jmeos.types.time.Period;
+import jmeos.types.time.PeriodSet;
+import org.apache.spark.sql.*;
+import org.apache.spark.sql.types.StructType;
+import org.apache.spark.sql.types.UDTRegistration;
+import org.mobiltydb.UDT.PeriodSetUDT;
+import org.mobiltydb.UDT.PeriodUDT;
+
+import java.sql.SQLException;
+import java.time.OffsetDateTime;
+import java.util.List;
+
+import static jmeos.functions.functions.meos_finalize;
+import static jmeos.functions.functions.meos_initialize;
+
+public class PeriodSetExample {
+    public static void main(String[] args) throws SQLException, AnalysisException {
+        meos_initialize("UTC");
+
+        SparkSession spark = SparkSession
+                .builder()
+                .appName("Java Spark SQL basic example")
+                .config("spark.master", "local")
+                .getOrCreate();
+
+        UDTRegistration.register(Period.class.getCanonicalName(), PeriodUDT.class.getCanonicalName());
+        UDTRegistration.register(PeriodSet.class.getCanonicalName(), PeriodSetUDT.class.getCanonicalName());
+
+        // Create Period and PeriodSet objects
+        OffsetDateTime now = OffsetDateTime.now();
+        Period period1 = new Period("[2021-04-08 05:04:45+01, 2021-04-08 06:04:45+01]");
+        Period period2 = new Period("[2021-04-08 07:04:45+01, 2021-04-08 08:04:45+01]");
+        Period period3 = new Period("[2021-04-08 09:04:45+01, 2021-04-08 10:04:45+01]");;
+
+        PeriodSet periodSet = new PeriodSet(period1, period2, period3);
+
+        List<Row> data = List.of(
+                RowFactory.create(periodSet)
+        );
+
+        StructType schema = new StructType()
+                .add("periodSet", new PeriodSetUDT());
+
+        // Create a DataFrame with a single column of Periods
+        Dataset<Row> df = spark.createDataFrame(data, schema);
+
+        // Register the DataFrame as a temporary view
+        df.createOrReplaceTempView("PeriodSets");
+
+        // Use Spark SQL to query the view
+        Dataset<Row> result = spark.sql("SELECT * FROM PeriodSets");
+
+        // Show the result
+        result.show(false);
+
+        df.printSchema();
+
+        meos_finalize();
+        spark.stop();
+
+    }
+}
--- src/main/java/org/mobiltydb/UDT/PeriodSetUDT.java
+++ src/main/java/org/mobiltydb/UDT/PeriodSetUDT.java
@@ -1,43 +1,58 @@
 package org.mobiltydb.UDT;
 
+import jmeos.types.time.Period;
 import jmeos.types.time.PeriodSet;
 import org.apache.spark.sql.catalyst.InternalRow;
 import org.apache.spark.sql.catalyst.expressions.GenericInternalRow;
+import org.apache.spark.sql.catalyst.util.ArrayData;
 import org.apache.spark.sql.types.*;
 import org.apache.spark.unsafe.types.UTF8String;
 
 import java.sql.SQLException;
+import java.util.Arrays;
+import java.util.List;
 
 
 public class PeriodSetUDT extends UserDefinedType<PeriodSet> {
 
     @Override
     public StructType sqlType() {
-        return DataTypes.createStructType(new StructField[] {
-                DataTypes.createStructField("value", DataTypes.StringType, false)
-        });
+        return new StructType().add(
+                "value", new ArrayType(new PeriodUDT(),false)
+        );
     }
 
     @Override
     public PeriodSet deserialize(Object datum) {
-        if (datum instanceof InternalRow) {
-            InternalRow row = (InternalRow) datum;
-            try {
-                return new PeriodSet((String) row.get(0, DataTypes.StringType));
-            } catch (SQLException exception) {
-                return null;
+        if (datum instanceof InternalRow row) {
+            Period[] periods = new Period[row.numFields()];
+            for (int i = 0; i < periods.length; i++) {
+                periods[i] = (Period) row.get(i, new PeriodUDT());
             }
 
+            try {
+                return new PeriodSet(periods);
+            } catch (SQLException e) {
+                throw new RuntimeException(e);
+            }
         }
         return null;
     }
+
     @Override
     public Object serialize(PeriodSet periodSet) {
         if (periodSet == null) {
             return null;
         }
 
-        return new GenericInternalRow(new Object[] {periodSet.getValue()});
+        List<Period> periods = Arrays.stream(periodSet.periods()).toList();
+
+        Object[] values = new Object[periods.size()];
+        for(int i = 0; i < periods.size(); i++) {
+            values[i] = periods.get(i);
+        }
+
+        return new GenericInternalRow(values);
     }
 
     @Override
